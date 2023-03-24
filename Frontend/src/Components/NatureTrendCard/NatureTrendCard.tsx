59b4b739035a76c416ff43c16e0a5da163b98e2b
Alex Atsoak <atsoak alex>
2023-03-24 11:55:55 +0100

    components

---
+++ Frontend/src/Components/NatureTrendCard/NatureTrendCard.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const NatureTrendsCard = () => {
+  const [trends, setTrends] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getTrends = async () => {
+      const response = await axios.get(
+        'https://naturaldisaster2.p.rapidapi.com/',
+        {
+            headers: {
+                'X-RapidAPI-Key': '325a7d1da8msh3ab021574148a9dp120784jsn6047bac0408e',
+                'X-RapidAPI-Host': 'naturaldisaster2.p.rapidapi.com'
+              }
+        }
+      );
+      const trendsData = response.data[0].trends.filter((trend: any) =>
+        trend.name.toLowerCase().includes("nature")
+      );
+      const trendsNames = trendsData.map((trend: any) => trend.name);
+      setTrends(trendsNames);
+    };
+
+    getTrends();
+  }, []);
+
+  return (
+    <div className="max-w-sm mx-auto rounded-md overflow-hidden shadow-md bg-white  text-green-800">
+      <h2 className="text-xl font-semibold mb-4">Trending Topics</h2>
+      <ul>
+        {trends.map((trend) => (
+          <li key={trend} className="text-gray-600 mb-2">
+            {trend}
+          </li>
+          
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NatureTrendsCard;
---
+++ Frontend/src/Components/SideBar/SideBar.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import { SidebarData } from './SideBarData';
+
+
+const SideBar: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="container">
+      <div
+        style={{ width: isOpen ? '200px' : '50px' }}
+        className="sidebar bg-green-50 text-green-900 h-screen py-8 px-4 transition-all duration-500 ease-in-out"
+      >
+        <div className="top_section flex justify-between items-center mb-8">
+          <h1
+            style={{ display: isOpen ? 'block' : 'none' }}
+            className="logo text-xl font-bold"
+          >
+            Logo
+          </h1>
+          <div
+            style={{ marginLeft: isOpen ? '50px' : '0px' }}
+            className="bars cursor-pointer"
+          >
+            <FaBars onClick={toggle} />
+          </div>
+        </div >
+        {SidebarData.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link flex items-center py-2 px-4 hover:bg-green-700 hover:text-white"
+            activeClassName="active"
+          >
+            <div className="icon"  style={{ marginLeft: toggle ? '-18px' : '0px' }}     >{item.icon}</div>
+            <div
+              style={{ display: isOpen ? 'block' : 'none' }}
+              className="link_text ml-2"
+            >
+              {item.title}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main className="ml-0 sm:ml-50">{children}</main>
+    </div>
+  );
+};
+
+export default SideBar;
+
+
