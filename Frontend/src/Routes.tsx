6ea6bb3e5c765f7c78bb3c276d84a85dc13a081c
liolle <laurentlionel20@hotmail.com>
2023-03-30 13:37:08 +0200

    api doc in progress

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -12,6 +12,23 @@ import Login from "./Pages/Login"
 import NatureCard from "./Components/GalleryCard/GalleryCard"
 import NatureTrendCard from "./Components/NatureTrendCard/NatureTrendCard"
 import Publication from "./Components/Publication/Publication"
+import { APIDoc } from "./Pages/API"
+
+let example_User = {
+    id: 0,
+    author: {
+        name: "John Doe",
+        username: "johndoe",
+        avatarUrl: "https://randomuser.me/api/portraits/women/8.jpg"
+    },
+    content: "Ces services permettent de trouver les sources des photographies et donc de mieux comprendre les restrictions d’usage des images trouvées ici et là sur Internet. Dans tous les cas, méfiez-vous des images trop « parfaites » : généralement issues de banques d’images, leur utilisation est rarement gratuite !",
+    imageUrl: undefined,
+    videoUrl: undefined,
+    postedAt: "",
+    likes: 0,
+    shares: 0,
+    comments: 0
+}
 
 
 
@@ -24,28 +41,11 @@ export function routes() {
             <Route path='/post' element={<PostCard profilePictureUrl="https://randomuser.me/api/portraits/women/8.jpg" />} />
             <Route path='NatureCard/' element={<NatureCard />} />
             <Route path='NatureTrendCard/' element={<NatureTrendCard />} />
-            <Route path='Publication/' element={<Publication data={{
-            id: 0,
-            author: {
-                name: "John Doe",
-                username: "johndoe",
-                avatarUrl: "https://randomuser.me/api/portraits/women/8.jpg"
-            },
-            content: "Ces services permettent de trouver les sources des photographies et donc de mieux comprendre les restrictions d’usage des images trouvées ici et là sur Internet. Dans tous les cas, méfiez-vous des images trop « parfaites » : généralement issues de banques d’images, leur utilisation est rarement gratuite !",
-            imageUrl: undefined,
-            videoUrl: undefined,
-            postedAt: "",
-            likes: 0,
-            shares: 0,
-            comments: 0
-        }} />} />
-
-        
-       
-
+            <Route path='Publication/' element={<Publication data={example_User} />} />
             <Route path= '/Register' element={<Register/>} />
             <Route path= '/Login' element={<Login/>} />
           
+            <Route path= '/api-doc' element={<APIDoc/>} />
         
     </Routes>
 }
